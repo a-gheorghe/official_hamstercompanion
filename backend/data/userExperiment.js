5cf9e340c5fc4f4f0f2cdf96975bdaa575725a06
crawforde <crawforde@kenyon.edu>
2017-12-06 13:42:11 -0800

    Made dummy data files for user_experiment and user

---
+++ backend/data/userExperiment.js
@@ -0,0 +1,50 @@
+export default[
+  {
+    userId: 1,
+    experimentId: 1
+  },
+  {
+    userId: 2,
+    experimentId: 2
+  },
+  {
+    userId: 3,
+    experimentId: 3
+  },
+  {
+    userId: 4,
+    experimentId: 4
+  },
+  {
+    userId: 5,
+    experimentId: 5
+  },
+  {
+    userId: 6,
+    experimentId: 5
+  },
+  {
+    userId: 1,
+    experimentId: 4
+  },
+  {
+    userId: 1,
+    experimentId: 3
+  },
+  {
+    userId: 2,
+    experimentId: 1
+  },
+  {
+    userId: 2,
+    experimentId: 5
+  },
+  {
+    userId: 3,
+    experimentId: 5
+  },
+  {
+    userId: 5,
+    experimentId: 3
+  }
+];
--- backend/data/users.js
+++ backend/data/users.js
@@ -1,7 +1,42 @@
 export default [
   {
-
+    username: "a.gheorghe",
+    password: "password_a",
+    fName: "Ana",
+    lname: "Gheorghe",
+    sex: 'F'
   },
-
-
+  {
+    username: "crawforde",
+    password: "EillePassword",
+    fName: "Eliana",
+    lname: "Crawford",
+    sex: 'F',
+    email: 'l84swim@gmail.com'
+  },
+  {
+    username: "borajimin",
+    password: "password",
+    fName: "JiMin",
+    sex: 'F',
+    email: 'jimin@mathmemes.com'
+  },
+  {
+    username: "SlowBro",
+    fname: "Brock",
+    password: "password",
+    sex: 'M',
+    email: 'brock@brock.com'
+  },
+  {
+    username: "Pikachu",
+    password: "password"
+  },
+  {
+    username: "natedana",
+    fname: "Nate",
+    password: "password",
+    sex: 'M',
+    email: 'nate@horizons.com'
+  }
 ];
