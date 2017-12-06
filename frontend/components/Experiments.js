e8ec3b730d1f3d226309c0e58cd3c5728fe23dc8
Brock <bws219@lehigh.edu>
2017-12-05 17:00:36 -0800

    front end to back end skeleton ready

--- frontend/components/Experiments.js
+++ frontend/components/Experiments.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 class Experiments extends React.Component {
-  componentDidMount() {
-    // $.ajax('./getExperiments');
+  constructor(props) {
+    super(props);
+    this.state = {
+      exps: []
+    };
+  }
+  componentWillMount() {
+    axios.get('./getExperiments')
+      .then(resp => {
+        this.setState({exps: resp.data});
+      });
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={e => this.submit(e)}>
-          <input type="submit" />
-        </form>
+        {this.state.exps.map(() => {
+          // TODO render the exeriment
+        })}
       </div>
     );
   }
