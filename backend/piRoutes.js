0570b31ae0696ba4cf13e5215c2dd7ea9bf0530a
Brock <bws219@lehigh.edu>
2017-12-12 17:25:09 -0800

    prepare for pull

--- backend/piRoutes.js
+++ backend/piRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { Session } = require('./models');
+const { Session, Mouse } = require('./models');
 
 router.post('/hamster', (req, res) => {
   console.log(req.body);
   Session.create(req.body).then(resp => {
     console.log('RESPONSE', resp);
     res.send('Post request received!');
+  }).catch(e => console.log(e));
+});
+
+router.post('/new/mouse', (req, res) => {
+  console.log(req.body);
+  Mouse.create(req.body).then(resp => {
+    console.log('RESPONSE', resp);
+    res.send('New Mouse created!');
   }).catch(e => console.log(e));
 });
 
