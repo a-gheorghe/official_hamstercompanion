5c861d107b2ed77c9c2c715d4406d14593b80826
crawforde <crawforde@kenyon.edu>
2017-12-14 15:54:40 -0800

    Added middleware to the server to check if user has access to an experiment. Also added middleware further down the page to check if user has administrative rights over an experiment

--- backend/api.js
+++ backend/api.js
@@ -12,17 +12,75 @@ router.get('/experiments', (req, res) => {
   }).catch(e => console.log(e));
 });
 
+router.post('/experiment', (req, res) => {
+  Experiment.create(req.body)
+    .then(resp => UserExperiment.create({
+      userId: req.user.id,
+      experimentId: resp.id
+    })).then(resp => res.send({ success: true, response: resp }))
+    .catch(e => console.log(e));
+});
+
+router.post('/join/experiment', (req, res) => {
+  Experiment.findById(req.body.id, { include: {
+    model: UserExperiment,
+    required: false,
+    where: { userId: req.user.id }
+  } }).then(resp => {
+    if (!resp) {
+      res.json({
+        success: false,
+        error: "Experiment does not exist"
+      });
+    } else if (req.body.password !== resp.password) {
+      res.json({
+        success: false,
+        error: "Incorrect Password"
+      });
+    } else if (resp.user_experiments[0]) {
+      res.json({
+        success: false,
+        error: "You're already in this experiment"
+      });
+    } else {
+      UserExperiment.create({
+        userId: req.user.id,
+        experimentId: resp.id
+      }).then(() => res.json({ success: true }));
+    }
+  }).catch(e => console.log(e));
+});
+
+
+// MIDDLEWARE TO CHECK IF A USER HAS ACCESS TO A PARTICULAR EXPERIMENT. THE ADMIN STATUS OF THE USER FOR THE EXPERIMENT IS ALSO STORED AS req.admin
+router.use('/experiment/:id', (req, res, next) => {
+  UserExperiment.findOne({
+    where: {
+      experimentId: req.params.id,
+      userId: req.user.id
+    }
+  })
+  .then((resp)=>{
+    if(!resp.dataValues) {
+      res.send(false);
+    }
+    else{
+      req.isAdmin = resp.dataValues.isAdmin;
+      next();
+    }
+  })
+  .catch((err)=>{
+    console.log('Server Error');
+    res.status(500).send(err);
+  });
+});
+
 router.get('/experiment/:id', (req, res) => {
   var yesterday = new Date();
   yesterday = new Date(yesterday.setTime(yesterday.getTime() - 86400000));
   Experiment.findById(req.params.id, {
     attributes: ['id', 'createdAt', 'name', 'description', 'minDailySessions'],
     include: [
-      {
-        model: UserExperiment,
-        attributes: ['isAdmin'],
-        where: { userId: req.user.id },
-      },
       {
         model: TreatmentGroup,
         required: false,
@@ -65,8 +123,10 @@ router.get('/experiment/:id', (req, res) => {
         ]
       }
     ]}).then(resp => {
-      console.log('*******************', resp, '***************');
-      res.json(resp);
+      res.send({
+        experiment: resp,
+        isAdmin: req.isAdmin
+      });
     }).catch(e => console.log(e));
 });
 
@@ -95,43 +155,15 @@ router.get('/experiment/:id/sessions', (req, res)=>{
     });
 });
 
-router.post('/experiment', (req, res) => {
-  Experiment.create(req.body)
-    .then(resp => UserExperiment.create({
-      userId: req.user.id,
-      experimentId: resp.id
-    })).then(resp => res.send({ success: true, response: resp }))
-    .catch(e => console.log(e));
+// MIDDLEWARE TO CHECK IF USER HAS ADMINISTRATIVE RIGHTS OVER AN EXPERIMENT
+router.use('/experiment/:id', (req, res, next)=>{
+  if(req.isAdmin) {
+    next();
+  }
+  else{
+    res.status(400).send('You do not have administrative rights to this experiment.');
+  }
 });
 
-router.post('/join/experiment', (req, res) => {
-  Experiment.findById(req.body.id, { include: {
-    model: UserExperiment,
-    required: false,
-    where: { userId: req.user.id }
-  } }).then(resp => {
-    if (!resp) {
-      res.json({
-        success: false,
-        error: "Experiment does not exist"
-      });
-    } else if (req.body.password !== resp.password) {
-      res.json({
-        success: false,
-        error: "Incorrect Password"
-      });
-    } else if (resp.user_experiments[0]) {
-      res.json({
-        success: false,
-        error: "You're already in this experiment"
-      });
-    } else {
-      UserExperiment.create({
-        userId: req.user.id,
-        experimentId: resp.id
-      }).then(() => res.json({ success: true }));
-    }
-  }).catch(e => console.log(e));
-});
 
 module.exports = router;
--- frontend/components/Dashboard.js
+++ frontend/components/Dashboard.js
@@ -13,8 +13,10 @@ class Dashboard extends React.Component {
   }
   componentWillMount() {
     axios.get('/api/experiment/' + this.props.match.params.id).then(resp => {
-      console.log(resp.data);
-      this.setState({experiment: resp.data});
+      this.setState({
+        experiment: resp.data.experiment,
+        isAdmin: resp.data.isAdmin
+      });
     }).catch(e => console.log(e));
   }
 
@@ -26,7 +28,7 @@ class Dashboard extends React.Component {
           <div id="dashboard-info">
             <h3>Experiment ID: {this.state.experiment.id}</h3>
             <h3>Description: {this.state.experiment.description}</h3>
-            {this.state.experiment.user_experiments[0].isAdmin ?
+            {this.state.isAdmin ?
             (<Link to={`/experiment/${this.state.experiment.id}/edit`}><button>Edit Experiment</button></Link>) :
             (<button>Become Administrator</button>)
             }
