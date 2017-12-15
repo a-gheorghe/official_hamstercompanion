21b540c15d7d12241e6aca8f3c51db73b286b676
Brock <bws219@lehigh.edu>
2017-12-14 16:12:38 -0800

    first half of editing page done

--- frontend/components/Dashboard.js
+++ frontend/components/Dashboard.js
@@ -27,8 +27,8 @@ class Dashboard extends React.Component {
             <h3>Experiment ID: {this.state.experiment.id}</h3>
             <h3>Description: {this.state.experiment.description}</h3>
             {this.state.experiment.user_experiments[0].isAdmin ?
-            (<Link to={`/experiment/${this.state.experiment.id}/edit`}><button>Edit Experiment</button></Link>) :
-            (<button>Become Administrator</button>)
+              (<Link to={`/experiment/${this.state.experiment.id}/edit`}><button>Edit Experiment</button></Link>) :
+              (<button>Become Administrator</button>)
             }
           </div>
           <DashboardTable experiment={this.state.experiment}/>
--- frontend/components/EditExperiment.js
+++ frontend/components/EditExperiment.js
@@ -7,23 +7,50 @@ class EditExperiment extends React.Component {
     super(props);
     this.state = {
       submitted: false,
-      error: ''
+      error: '',
+      name: '',
+      desc: '',
+      edit: false,
     };
+  }
+
+  componentWillMount() {
+    if (this.props.match.params.id) {
+      axios.get('/api/experiment/' + this.props.match.params.id).then(resp => {
+        this.setState({
+          name: resp.data.name,
+          desc: resp.data.description,
+          edit: true
+        });
+      });
+    }
   }
 
   submit(e) {
     e.preventDefault();
-    if (e.target.adminPassword.value === e.target.adminPassRepeat.value) {
-      axios.post('/api/experiment', {
-        name: e.target.name.value,
-        password: e.target.password.value,
-        description: e.target.desc.value || null,
-        adminPassword: e.target.adminPassword.value
-      }).then(resp => {
-        if (resp.data.success) this.setState({ submitted: true });
-      }).catch(err => console.log(err));
-    } else this.setState({ error: 'Admin passwords do not match!'});
+    if (this.state.edit) {
+      if (!e.target.adminPasswordCheck.value) {
+        this.setState({ error: 'Admin password required to edit' });
+      } else {
+        // TODO EDIT EXPERIMENT ON SERVER SIDE
+      }
+    } else {
+      if (e.target.adminPassword.value === e.target.adminPassRepeat.value) {
+        axios.post('/api/experiment', {
+          name: e.target.name.value,
+          password: e.target.password.value,
+          description: e.target.desc.value || null,
+          adminPassword: e.target.adminPassword.value
+        }).then(resp => {
+          if (resp.data.success) this.setState({ submitted: true });
+        }).catch(err => console.log(err));
+      } else this.setState({ error: 'Admin passwords do not match!'});
+    }
   }
+
+  changeName(e) { this.setState({ name: e.target.value }); }
+
+  changeDesc(e) { this.setState({ desc: e.target.value }); }
 
   render() {
     if (this.state.submitted) return <Redirect to="/" />;
@@ -31,13 +58,23 @@ class EditExperiment extends React.Component {
     return (
       <div>
         <h3>Create or edit an Experiment</h3>
+        { this.state.error ? <p style={{ color: 'red' }}>{this.state.error}</p> : null }
         <form className="col form" onSubmit={e => this.submit(e)}>
-          <input type="text" name="name" placeholder="Experiment Name" />
+          <input type="text" name="name" onChange={e => this.changeName(e)}
+            placeholder="Experiment Name" value={this.state.name}
+          />
           <input type="password" name="password" placeholder="Password to Join Experiment" />
-          <input type="text" name="desc" placeholder="Description of your experiment" />
-          { this.state.error ? <p style={{ color: 'red' }}>{this.state.error}</p> : null }
+          <input type="text" name="desc" onChange={e => this.changeDesc(e)}
+            placeholder="Description of your experiment" value={this.state.desc}
+          />
           <input type="password" name="adminPassword" placeholder="Admin Password" />
           <input type="password" name="adminPassRepeat" placeholder="Repeat Admin Password" />
+          { this.state.edit ?
+            <p>In order to edit this experimgnt you must enter the admin password:</p>
+            : null }
+          { this.state.edit ?
+            <input type="password" name="adminPasswordCheck" />
+            : null }
           <input type="submit" />
         </form>
         <Link className="box btn" to="/">Cancel</Link>
